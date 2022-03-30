de6d1706e0e6c6e5eda1c5450b910925e0b55952
Md Murad Hossain <mdmuradhossain33@gmail.com>
2022-03-30 19:48:28 +0600

    binary-tree implementation added

---
+++ Tree Data Structure/src/io/murad/binarytree/BinaryTree.java
@@ -0,0 +1,59 @@
+package io.murad.binarytree;
+
+public class BinaryTree {
+
+	Node root;
+
+	public BinaryTree(int key) {
+		root = new Node(key);
+	}
+
+	public BinaryTree() {
+		root = null;
+	}
+
+	// Traverse inorder
+	public void traverseInOrder(Node node) {
+		if (node != null) {
+			traverseInOrder(node.left);
+			System.out.println(" " + node.key);
+			traverseInOrder(node.right);
+		}
+	}
+
+	// Traverse postorder
+	public void traversePostOrder(Node node) {
+		if (node != null) {
+			traversePostOrder(node.left);
+			traversePostOrder(node.right);
+			System.out.println(" " + node.key);
+		}
+	}
+
+	// Traverse preorder
+	public void traversePreOrder(Node node) {
+		if (node != null) {
+			System.out.println(" " + node.key);
+			traversePreOrder(node.left);
+			traversePreOrder(node.right);
+		}
+	}
+
+	public static void main(String[] args) {
+		BinaryTree tree = new BinaryTree();
+
+		tree.root = new Node(1);
+		tree.root.left = new Node(2);
+		tree.root.right = new Node(3);
+		tree.root.left.left = new Node(4);
+
+		System.out.print("Pre order Traversal: ");
+		tree.traversePreOrder(tree.root);
+		System.out.print("\nIn order Traversal: ");
+		tree.traverseInOrder(tree.root);
+		System.out.print("\nPost order Traversal: ");
+		tree.traversePostOrder(tree.root);
+
+	}
+
+}
---
+++ Tree Data Structure/src/io/murad/binarytree/Node.java
@@ -0,0 +1,12 @@
+package io.murad.binarytree;
+
+public class Node {
+
+	int key;
+	Node left, right;
+	
+	public Node(int item) {
+		key = item;
+		left = right = null;
+	}
+}
