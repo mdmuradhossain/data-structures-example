fe1da37cd905b45d0a4581c0b6e183543caa2ec5
Md Murad Hossain <mdmuradhossain33@gmail.com>
2022-04-01 13:10:15 +0600

    array readme updated

---
+++ Array Data Structure/README.md
@@ -0,0 +1,24 @@
+# Array Data Structure
+
+An array is a linear data structure that collects elements of the same data type and stores them in contiguous and adjacent memory locations.
+Arrays work on an index system starting from `0` to `(n-1)`, where n is the size of the array.
+
+## Types of Array
+
+### One-dimensional Array
+In a one-dimensional array the elements are stored in contiguous memory locations where each element is accessed by using a single index value. It is a linear data structure storing all the elements in sequence.
+
+### Two-dimensional Array
+In types of arrays, a two dimensional array is a tabular representation of data where elements are stored in rows and columns. A two dimensional array is actually a collection of M X N elements which has M rows and N columns.  To access any element in a two-dimensional array two subscripts are required for defining position of an element in a specific row and column. The first index is for row number and second is for column index.
+
+### Three-dimensional Array
+In types of arrays, a three-dimensional array is an extension to the two dimensional array with addition of depth. It can be seen as a cube that has rows, columns and depth as third dimension. To access any element in a three-dimensional array three subscripts are required for position of element in a specific row, column and depth. The first index is for depth (dimension or layer), second is for row index and third is for column.
+
+
+## What Operations Can You Perform on an Array?
+
+- Traversal
+- Insertion
+- Deletion
+- Searching
+- Sorting
--- Array Data Structure/src/io/murad/array/Array.java
+++ Array Data Structure/src/io/murad/array/Array.java
@@ -1,29 +1,59 @@
 package io.murad.array;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Array {
-	
-	public static void main(String[] args) {
-		
-		Scanner userInput = new Scanner(System.in);
-		
-		int arr[] = new int[]{1,3,4,7,8,9};
-		int arr2[]= new int[5];
-		int arr3[] = {2,7,4,5,3,1};
-		int arr4[];  // Declaring an array
-		int arrayLength = userInput.nextInt();
-		
-		arr4 = new int[arrayLength];// Allocating memory to the array
-		
-		//Inserting an array
-		for(int i=0; i<arr4.length;i++) {
-			System.out.println("Input number: ");
-			arr4[i] = userInput.nextInt();
-		}
 
-		
-		userInput.close();
+	public static int sumArray(List<Integer> arr) {
+		int sum = 0;
+		for (Integer a : arr) {
+			sum = sum + a;
+//		     0 = 0 + 1;
+//		     1 = 1 + 2;
+//		     3 = 3 + 3;
+//		     6 = 6 + 4;
+//		     10 = 10 + 10;
+//		     20 = 20 + 11;
+//		     sum = 31
+		}
+		System.out.println(sum);
+		return sum;
+	}
+
+	public static void main(String[] args) {
+
+//		Scanner userInput = new Scanner(System.in);
+//
+//		int arr[] = new int[] { 1, 3, 4, 7, 8, 9 };
+//		int arr2[] = new int[5];
+//		int arr3[] = { 2, 7, 4, 5, 3, 1 };
+//		int arr4[]; // Declaring an array
+//		System.out.println("Insert Array Size: ");
+//		int arrayLength = userInput.nextInt();
+//
+//		arr4 = new int[arrayLength];// Allocating memory to the array
+//
+//		// Inserting an array
+//		for (int i = 0; i < arr4.length; i++) {
+//			System.out.println("Input number: ");
+//			arr4[i] = userInput.nextInt();
+//		}
+//
+//		for (int i = 0; i < arr4.length; i++) {
+//			System.out.println("Traverse Array: " + arr4[i]);
+//		}
+
+		List<Integer> arrSum = new ArrayList<Integer>();
+		arrSum.add(0, 1);
+		arrSum.add(1, 2);
+		arrSum.add(2, 3);
+		arrSum.add(3, 4);
+		arrSum.add(4, 10);
+		arrSum.add(5, 11);
+		Array.sumArray(arrSum);
+//		userInput.close();
 	}
 
 }
