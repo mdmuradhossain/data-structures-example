84e12b6038406a02d88aab5797cc7c7669f3c84e
Md Murad Hossain <mdmuradhossain33@gmail.com>
2022-04-06 23:17:38 +0600

    search from array updated

--- Array Data Structure/src/io/murad/array/Array.java
+++ Array Data Structure/src/io/murad/array/Array.java
@@ -22,28 +22,36 @@ public class Array {
 		return sum;
 	}
 
+	static int findElement(int arr[], int n, int key) {
+		for (int i = 0; i < n; i++)
+			if (arr[i] == key)
+				return i;
+
+		return -1;
+	}
+
 	public static void main(String[] args) {
 
-//		Scanner userInput = new Scanner(System.in);
-//
-//		int arr[] = new int[] { 1, 3, 4, 7, 8, 9 };
-//		int arr2[] = new int[5];
-//		int arr3[] = { 2, 7, 4, 5, 3, 1 };
-//		int arr4[]; // Declaring an array
-//		System.out.println("Insert Array Size: ");
-//		int arrayLength = userInput.nextInt();
-//
-//		arr4 = new int[arrayLength];// Allocating memory to the array
-//
-//		// Inserting an array
-//		for (int i = 0; i < arr4.length; i++) {
-//			System.out.println("Input number: ");
-//			arr4[i] = userInput.nextInt();
-//		}
-//
-//		for (int i = 0; i < arr4.length; i++) {
-//			System.out.println("Traverse Array: " + arr4[i]);
-//		}
+		Scanner userInput = new Scanner(System.in);
+
+		int arr[] = new int[] { 1, 3, 4, 7, 8, 9 };
+		int arr2[] = new int[5];
+		int arr3[] = { 2, 7, 4, 5, 3, 1 };
+		int arr4[]; // Declaring an array
+		System.out.println("Insert Array Size: ");
+		int arrayLength = userInput.nextInt();
+
+		arr4 = new int[arrayLength];// Allocating memory to the array
+
+		// Inserting an array
+		for (int i = 0; i < arr4.length; i++) {
+			System.out.println("Input number: ");
+			arr4[i] = userInput.nextInt();
+		}
+
+		for (int i = 0; i < arr4.length; i++) {
+			System.out.println("Traverse Array: " + arr4[i]);
+		}
 
 		List<Integer> arrSum = new ArrayList<Integer>();
 		arrSum.add(0, 1);
