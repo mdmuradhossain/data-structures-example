54de1de3659b6bc0c5bf336a7190305a2d582582
Md Murad Hossain <mdmuradhossain33@gmail.com>
2022-04-08 16:22:38 +0600

    array code updated with delete operation

--- Array Data Structure/src/io/murad/array/Array.java
+++ Array Data Structure/src/io/murad/array/Array.java
@@ -31,6 +31,48 @@ public class Array {
 		}
 	}
 
+	static int findElement(int arr[], int n, int key) {
+		for (int i = 0; i < n; i++) 
+			if (arr[i] == key) 
+				return i;
+		
+		return -1;
+	}
+	// Function to insert a given key in
+	// the array. This function returns n+1
+	// if insertion is successful, else n.
+	static int insertSorted(int arr[], int n, int key, int capacity) {
+
+		// Cannot insert more elements if n
+		// is already more than or equal to
+		// capacity
+		if (n >= capacity)
+			return n;
+
+		arr[n] = key;
+
+		return (n + 1);
+	}
+
+	// Function to delete an element
+	static int deleteElement(int arr[], int n, int key) {
+		// Find position of element to be
+		// deleted
+		int pos = findElement(arr, n, key);
+
+		if (pos == -1) {
+			System.out.println("Element not found");
+			return n;
+		}
+
+		// Deleting element
+		int i;
+		for (i = pos; i < n - 1; i++)
+			arr[i] = arr[i + 1];
+
+		return n - 1;
+	}
+
 	public static void main(String[] args) {
 
 		Scanner userInput = new Scanner(System.in);
